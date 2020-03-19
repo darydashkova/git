6552128aa644fcdcc0f4e68d32115d335174223d
Dasha <dashkovad96@mail.ru>
2020-03-19 13:36:01 +0300

    19.03.2020

--- src/gui.ts
+++ src/gui.ts
@@ -1,12 +1,22 @@
 import * as $ from 'jquery';
 // import '../node_modules/select2/dist/js/select2.full';
-
+const submit = document.querySelector('.slider-container__back');
+let show = true;
 export function guiInit() {
   // Проверка страницы
   if ($('.gui-page').toArray().length) {
     // Подключение селекта
     // $('#gui-select_1').select2({ minimumResultsForSearch: Infinity, dropdownCssClass: 'form-element__control-select-dropdown' });
-
     console.log('Gui page');
+
+    submit.addEventListener('click', function () {
+      const fff = document.querySelector('.table__item');
+      if (show) {
+        fff.classList.add('none');
+      } else {
+        fff.classList.remove('none');
+      }
+      show=!show;
+    });
   }
 }
