c62c9a262b750584ba8dd471cc9ad1e5270ef21b
Dasha <dashkovad96@mail.ru>
2020-03-22 17:27:06 +0300

    slider

--- src/gui.ts
+++ src/gui.ts
@@ -2,13 +2,14 @@ import * as $ from 'jquery';
 // import '../node_modules/select2/dist/js/select2.full';
 const submit = document.querySelector('.slider-container__back');
 let show = true;
+let summ = 1;
+const slider = document.querySelector('.slider__back');
 export function guiInit() {
   // Проверка страницы
   if ($('.gui-page').toArray().length) {
     // Подключение селекта
     // $('#gui-select_1').select2({ minimumResultsForSearch: Infinity, dropdownCssClass: 'form-element__control-select-dropdown' });
     console.log('Gui page');
-
     submit.addEventListener('click', function () {
       const fff = document.querySelector('.table__item');
       if (show) {
@@ -16,7 +17,41 @@ export function guiInit() {
       } else {
         fff.classList.remove('none');
       }
-      show=!show;
+      show = !show;
+    });
+    slider.addEventListener('click', function () {
+      const nextSlider = document.querySelector('.slider__container-img');
+      const secondSlide = document.querySelector('.slider__container-second');
+      const thirdlide = document.querySelector('.slider__container-third');
+      if (nextSlider) {
+        nextSlider.classList.remove('slider__container-img');
+        nextSlider.classList.add('addopacity');
+        secondSlide.classList.remove('slider__container-second');
+        secondSlide.classList.add('slider__container-img');
+        thirdlide.classList.remove('slider__container-third');
+        thirdlide.classList.add('slider__container-second');
+      } else {
+      }
+
+      if (summ == 2) {
+        nextSlider.classList.remove('slider__container-img');
+        nextSlider.classList.add('addopacity');
+        secondSlide.classList.remove('slider__container-second');
+        secondSlide.classList.add('slider__container-img');
+        thirdlide.classList.remove('slider__container-third');
+        thirdlide.classList.add('slider__container-second');
+        summ++;
+      }
+      if (summ == 3) {
+        nextSlider.classList.remove('slider__container-img');
+        nextSlider.classList.add('addopacity');
+        secondSlide.classList.remove('slider__container-second');
+        secondSlide.classList.add('slider__container-img');
+        thirdlide.classList.remove('slider__container-third');
+        thirdlide.classList.add('slider__container-second');
+        summ = 1;
+      }
+      summ++;
     });
   }
 }
