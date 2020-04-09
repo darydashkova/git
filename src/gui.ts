3399cf7c65f06df3df6141838cd6cb05979eeb81
Dasha <dashkovad96@mail.ru>
2020-04-09 18:09:47 +0300

    09/04

--- src/gui.ts
+++ src/gui.ts
@@ -1,31 +1,21 @@
 import * as $ from 'jquery';
+import Swiper from 'swiper';
 // import '../node_modules/select2/dist/js/select2.full';
-const submit = document.querySelector('.slider-container__back');
-const ru = document.querySelector('я');
-const eng = document.querySelector('.header__container-flex-lang-eng');
-let show = true;
 export function guiInit() {
   // Проверка страницы
   if ($('.gui-page').toArray().length) {
+    const swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
     // Подключение селекта
     // $('#gui-select_1').select2({ minimumResultsForSearch: Infinity, dropdownCssClass: 'form-element__control-select-dropdown' });
     console.log('Gui page');
-    submit.addEventListener('click', function () {
-      const fff = document.querySelector('.table__item');
-      if (show) {
-        fff.classList.add('none');
-      } else {
-        fff.classList.remove('none');
-      }
-      show = !show;
-    });
-    eng.addEventListener('click', function () {
-        eng.classList.add('header__container-flex-lang-active');
-        ru.classList.remove('header__container-flex-lang-active');
-    });
-    ru.addEventListener('click', function () {
-      ru.classList.add('header__container-flex-lang-active');
-      eng.classList.remove('header__container-flex-lang-active');
-  });
   }
 }
--- src/partials/slider/slider.ts
+++ src/partials/slider/slider.ts
@@ -1,1 +1,12 @@
 import * as $ from 'jquery';
+import Swiper from 'swiper';
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
